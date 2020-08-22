23f525f43ed3e6a938a37520f4f318b5eefd03d6
Favna <contact@skyra.pw>
2020-08-22 01:55:47 +0000

    build: ts-config build for 991de0da0f9b51813f4fee4ecf071feeb9d95abe

--- dist/lib/isFunction.d.ts
+++ dist/lib/isFunction.d.ts
@@ -2,5 +2,5 @@
  * Verify if the input is a function.
  * @param input The function to verify
  */
-export declare function isFunction(input: unknown): input is (...args: unknown[]) => unknown;
+export declare function isFunction(input: unknown): input is Function;
 //# sourceMappingURL=isFunction.d.ts.map
