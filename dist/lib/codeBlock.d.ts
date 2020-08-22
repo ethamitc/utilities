6c774e5804ef55c5dbb7243ecee26ece9c7dc91a
Favna <contact@skyra.pw>
2020-08-22 00:40:30 +0000

    build: ts-config build for 9bc1f233c9979b22b34f26a5ad0efe083c376e88

---
+++ dist/lib/codeBlock.d.ts
@@ -0,0 +1,8 @@
+/**
+ * Wraps text in a markdown codeblock with optionally a language indicator for syntax highlighting
+ * @param expression The expression to be wrapped in the codeblock
+ * @param language The codeblock language
+ * @default 'md'
+ */
+export declare function codeBlock(expression: string, language?: string): string;
+//# sourceMappingURL=codeBlock.d.ts.map
