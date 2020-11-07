5e348af8bf34e57107480636d0567fbb5aa8238d
Favna <contact@skyra.pw>
2020-11-07 21:39:39 +0000

    build: time-utilities build for a2c68d84bb204a43ff599d5a24f85cb2b5be5ba8

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -3,4 +3,5 @@ export * from './lib/Cron';
 export * from './lib/Duration';
 export * from './lib/DurationFormatter';
 export * from './lib/Timestamp';
+export * from './lib/TimerManager';
 //# sourceMappingURL=index.d.ts.map
