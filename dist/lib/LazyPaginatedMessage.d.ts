e65cc1055f62dcc4b60632462dd2b870f0b09f6e
Favna <contact@skyra.pw>
2021-01-13 21:04:50 +0000

    build: discord.js-utilities build for 8d99797968af72fa02958d80eebcfc92a1cb3c2d

--- dist/lib/LazyPaginatedMessage.d.ts
+++ dist/lib/LazyPaginatedMessage.d.ts
@@ -1,7 +1,17 @@
 /// <reference types="discord.js" />
 import { PaginatedMessage } from './PaginatedMessage';
+/**
+ * This is a LazyPaginatedMessage. Instead of resolving all pages that are functions on [[PaginatedMessage.run]] will resolve when requested.
+ */
 export declare class LazyPaginatedMessage extends PaginatedMessage {
+    /**
+     * Only resolves the page corresponding with the handler's current index.
+     */
     resolvePagesOnStart(): Promise<void>;
+    /**
+     * Resolves the page corresponding with the given index. This also resolves the index's before and after the given index.
+     * @param index The index to resolve. Defaults to handler's current index.
+     */
     resolvePage(index?: number): Promise<import("discord.js").APIMessage | null>;
 }
 //# sourceMappingURL=LazyPaginatedMessage.d.ts.map
--- dist/lib/PaginatedMessage.d.ts
+++ dist/lib/PaginatedMessage.d.ts
@@ -1,29 +1,166 @@
 import type { APIMessage, User, TextChannel, NewsChannel, Message, ReactionCollector } from 'discord.js';
+/**
+ * This is a [[PaginatedMessage]], a utility to paginate messages (usually embeds).
+ * You must either use this class directly or extend it.
+ *
+ * [[PaginatedMessage]] uses actions, these are essentially reaction emojis, when triggered run the said action.
+ * You can utilize your own actions, or you can use the [[PaginatedMessage.defaultActions]].
+ * [[PaginatedMessage.defaultActions]] is also static so you can modify these directly.
+ *
+ * [[PaginatedMessage]] also uses pages, these are simply {@link https://discord.js.org/#/docs/main/stable/class/APIMessage APIMessages}.
+ *
+ * @example
+ * ```typescript
+ * const handler = new PaginatedMessage();
+ * ```
+ *
+ * @example
+ * ```typescript
+ * // To utilize actions you can use the IPaginatedMessageAction by implementing it into a class.
+ * // [[PaginatedMessage]] requires you to have the class initialized using `new`.
+ *
+ * class ForwardAction implements IPaginatedMessageAction {
+ *   public id = '▶️';
+ *
+ *   public run({ handler }) {
+ *     if (handler.index !== handler.pages.length - 1) ++handler.index;
+ *   }
+ * }
+ *
+ * // You can also give the object directly.
+ *
+ * const StopAction: IPaginatedMessageAction {
+ *   id: '⏹️',
+ *   disableResponseEdit: true,
+ *   run: ({ response, collector }) => {
+ *     await response!.reactions.removeAll();
+ *     collector!.stop();
+ *   }
+ * }```
+ *
+ */
 export declare class PaginatedMessage {
+    /**
+     * The pages to be converted to [[PaginatedMessage.messages]]
+     */
     pages: MessagePage[];
+    /**
+     * The pages which were converted from [[PaginatedMessage.pages]]
+     */
     messages: (APIMessage | null)[];
+    /**
+     * The actions which are to be used.
+     */
     actions: Map<string, IPaginatedMessageAction>;
+    /**
+     * The handler's current page/message index.
+     */
     index: number;
+    /**
+     * The amount of time to idle before the paginator is closed. Defaults to `20 * 1000`.
+     */
     idle: number;
+    /**
+     * Constructor for the [[PaginatedMessage]] class
+     * @param __namedParameters The [[PaginatedMessageOptions]] for this instance of the [[PaginatedMessage]] class
+     */
     constructor({ pages, actions }?: PaginatedMessageOptions);
+    /**
+     * Sets the handler's current page/message index.
+     * @param index The number to set the index to.
+     */
     setIndex(index: number): this;
+    /**
+     * Sets the amount of time to idle before the paginator is closed.
+     * @param idle The number to set the idle to.
+     */
     setIdle(idle: number): this;
+    /**
+     * Clears all current actions and sets them. The order given is the order they will be used.
+     * @param actions The actions to set.
+     */
     setActions(actions: IPaginatedMessageAction[]): this;
+    /**
+     * Adds actions to the existing ones. The order given is the order they will be used.
+     * @param actions The actions to add.
+     */
     addActions(actions: IPaginatedMessageAction[]): this;
+    /**
+     * Adds an action to the existing ones. This will be added as the last action.
+     * @param action The action to add.
+     */
     addAction(action: IPaginatedMessageAction): this;
+    /**
+     * Clears all current pages and messages and sets them. The order given is the order they will be used.
+     * @param pages The pages to set.
+     */
     setPages(pages: MessagePage[]): this;
+    /**
+     * Add pages to the existing ones. The order given is the order they will be used.
+     * @param pages The pages to add.
+     */
     addPages(pages: MessagePage[]): this;
+    /**
+     * Adds a page to the existing ones. This will be added as the last page.
+     * @param page The page to add.
+     */
     addPage(page: MessagePage): this;
+    /**
+     * This executes the [[PaginatedMessage]] and sends the pages corresponding with [[PaginatedMessage.index]].
+     * The handler will start collecting reactions and running actions once all actions have been reacted to the message.
+     * @param author The author to validate.
+     * @param channel The channel to use.
+     */
     run(author: User, channel: TextChannel | NewsChannel): Promise<this>;
+    /**
+     * This function is executed on [[PaginatedMessage.run]]. This is an extendable method.
+     */
     resolvePagesOnRun(): Promise<void>;
+    /**
+     * This function is executed whenever an action is triggered and resolved.
+     * @param index The index to resolve.
+     */
     resolvePage(index?: number): Promise<APIMessage | null>;
+    /**
+     * This clones the current handler into a new instance.
+     */
     clone(): PaginatedMessage;
+    /**
+     * The default actions of this handler.
+     */
     static defaultActions: IPaginatedMessageAction[];
 }
+/**
+ * @example
+ * ```typescript
+ * // To utilize actions you can use the [[IPaginatedMessageAction]] by implementing it into a class.
+ *
+ * class ForwardAction implements IPaginatedMessageAction {
+ *   public id = '▶️';
+ *
+ *   public run({ handler }) {
+ *     if (handler.index !== handler.pages.length - 1) ++handler.index;
+ *   }
+ * }
+ *
+ * // You can also give the object directly.
+ *
+ * const StopAction: IPaginatedMessageAction {
+ *   id: '⏹️',
+ *   disableResponseEdit: true,
+ *   run: ({ response, collector }) => {
+ *     await response!.reactions.removeAll();
+ *     collector!.stop();
+ *   }
+ * }```
+ */
 export interface IPaginatedMessageAction {
     id: string;
     run(context: PaginatedMessageActionContext): Awaited<unknown>;
 }
+/**
+ * The context to be used in [[IPaginatedMessageAction]].
+ */
 export interface PaginatedMessageActionContext {
     handler: PaginatedMessage;
     author: User;
@@ -35,6 +172,28 @@ export interface PaginatedMessageOptions {
     pages?: MessagePage[];
     actions?: IPaginatedMessageAction[];
 }
+/**
+ * The pages that are used for [[PaginatedMessage.pages]]
+ *
+ * Pages can be either an {@link https://discord.js.org/#/docs/main/stable/class/APIMessage APIMessage} directly,
+ * or an awaited function which returns an {@link https://discord.js.org/#/docs/main/stable/class/APIMessage APIMessage}.
+ *
+ * @example
+ * ```typescript
+ * // Direct usage as an APIMessage
+ *
+ * new APIMessage(message.channel, {
+ *   context: 'Test content!',
+ * });
+ *
+ * // An awaited function. This function also passes index, pages, and handler.
+ *
+ * (index, pages) =>
+ *   new APIMessage(message.channel, {
+ *     embed: new MessageEmbed().setFooter(`Page ${index + 1} / ${pages.length}`)
+ *   });
+ * ```
+ */
 export declare type MessagePage = ((index: number, pages: MessagePage[], handler: PaginatedMessage) => Awaited<APIMessage>) | APIMessage;
 declare type Awaited<T> = PromiseLike<T> | T;
 export {};
